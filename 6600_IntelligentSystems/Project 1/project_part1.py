6d3424652b1c76d81258200ea442c7e5951f4b95
RealDense <rtdensley@gmail.com>
2019-10-23 22:56:17 -0600

    IS project 1 init

---
+++ 6600_IntelligentSystems/Project 1/project_part1.py
@@ -0,0 +1,26 @@
+#/usr/bin/python
+
+################################################################
+# Riley Densley
+# A01227345
+# Project 1.1
+################################################################
+
+import cv2
+
+import json
+import random
+import sys
+
+
+# Third-party libraries
+import numpy as np
+import scipy as sp
+#import matplotlib.pyplot as plt
+
+# Other Imports
+# from mnist_loader import load_data_wrapper
+import network2
+import pickle as cPickle
+
+
